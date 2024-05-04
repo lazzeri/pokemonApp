3c40c97595b8866eecb2160e212751dcee0d8d55
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2024-05-04 13:51:11 +0200

    Main Init of base components, routing, tailwind and json server

--- pokemonApp/vite.config.js
+++ pokemonApp/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4200,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: ((path) => path.replace(/^\/api/, ''))
+      }
+    }
+  }
 })
